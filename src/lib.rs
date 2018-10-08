990f62fda89475857e17c9757b7cb87a55f74532
Serejka <5125402@mail.ru>
2018-10-08 18:17:37 +0300

    added crud default repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(proc_macro_derive_resolution_fallback)]
+
 extern crate futures;
 #[macro_use]
 extern crate failure;
--- src/repos/mod.rs
+++ src/repos/mod.rs
@@ -1,11 +1,13 @@
 //! Repos is a module responsible for interacting with postgres db
 
 pub mod error;
+pub mod repo;
 pub mod repo_factory;
 pub mod types;
 pub mod users;
 
 pub use self::error::*;
+pub use self::repo::*;
 pub use self::repo_factory::*;
 pub use self::types::*;
 pub use self::users::*;
