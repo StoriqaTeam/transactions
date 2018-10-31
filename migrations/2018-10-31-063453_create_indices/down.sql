3dcb8a18a8c23f911f071bdc5ed2f8712e426a0a
AlexeyKarasev <karasev.alexey@gmail.com>
2018-10-31 09:44:12 +0300

    added db indexes

---
+++ migrations/2018-10-31-063453_create_indices/down.sql
@@ -0,0 +1,5 @@
+DROP INDEX seen_hashes_hash_idx;
+DROP INDEX accounts_address_idx;
+DROP INDEX blockchain_transactions_hash_idx;
+DROP INDEX transactions_dr_account_id_idx;
+DROP INDEX transactions_cr_account_id_idx;
