50c3befb080cf0ea57199386b6d0c3eaa6265169
AlexeyKarasev <karasev.alexey@gmail.com>
2018-11-29 19:39:40 +0300

    2nd fix for 2 addrs in blockchain tx

--- src/repos/seen_hashes.rs
+++ src/repos/seen_hashes.rs
@@ -9,6 +9,7 @@ use schema::seen_hashes::dsl::*;
 
 pub trait SeenHashesRepo: Send + Sync + 'static {
     fn create(&self, payload: NewSeenHashes) -> RepoResult<SeenHashes>;
+    fn upsert(&self, payload: NewSeenHashes) -> RepoResult<SeenHashes>;
     fn get(&self, hash_: BlockchainTransactionId, currency_: Currency) -> RepoResult<Option<SeenHashes>>;
 }
 
@@ -27,6 +28,20 @@ impl SeenHashesRepo for SeenHashesRepoImpl {
                 })
         })
     }
+    fn upsert(&self, payload: NewSeenHashes) -> RepoResult<SeenHashes> {
+        with_tls_connection(|conn| {
+            diesel::insert_into(seen_hashes)
+                .values(payload.clone())
+                .on_conflict(hash)
+                .do_nothing()
+                .get_result::<SeenHashes>(conn)
+                .map_err(move |e| {
+                    let error_kind = ErrorKind::from(&e);
+                    ectx!(err e, error_kind => payload)
+                })
+        })
+    }
+
     fn get(&self, hash_: BlockchainTransactionId, currency_: Currency) -> RepoResult<Option<SeenHashes>> {
         with_tls_connection(|conn| {
             seen_hashes
